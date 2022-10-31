d859d48bbfa53505ccbdd512a40891610d9917ce
Ravi Vanam <ravivanam9922@gmail.com>
2022-10-31 20:11:44 +0530

    restructure to make moved reducer function less complex

--- src/reducers.js
+++ src/reducers.js
@@ -10,34 +10,20 @@ export function gridReducer(grid, action) {
             return { ...grid, endGame: false };
         }
         case 'moved': {
-            const [i, j] = grid.snake[grid.snake.length - 1];
-            const newPostion = getNextPostion(i, j, grid.cells.length, grid.cells[0].length, grid.direction);
+            const currentPostion = grid.snake[grid.snake.length - 1];
+            const nextPosition = getNextPosition(currentPostion[0], currentPostion[1], grid.cells.length, grid.cells[0].length, grid.direction);
 
-            // if newPostion is snake itself or a mine then end the game
-            if (grid.cells[newPostion[0]][newPostion[1]] === 'snake' || grid.cells[newPostion[0]][newPostion[1]] === 'mine')
-                return { ...grid, endGame: true, firstGame: false, gameOverScreen: true };
+            if (isSnakeGoingToDie(grid.cells, nextPosition))
+                return {
+                    ...grid,
+                    endGame: true,
+                    firstGame: false,
+                    gameOverScreen: true
+                };
 
-            const isFood = grid.cells[newPostion[0]][newPostion[1]] === 'food';
-
-            // remove the tail. but if snake ate food don't remove it
-            const newSnake = grid.snake.filter((pos, i) => (i !== 0 || isFood));
-
-            // if food then remove it cuz the snake ate it.
-            const newFood = grid.food.filter(pos => (pos[0] !== newPostion[0]) || (pos[1] !== newPostion[1]));
-
-            newSnake.push(newPostion);
-            const newCells = grid.cells.map((cellRows) => cellRows.map(() => 'empty'));
-
-            // update cells
-            newSnake.forEach(pos => {
-                newCells[pos[0]][pos[1]] = 'snake';
-            });
-            newFood.forEach(pos => {
-                newCells[pos[0]][pos[1]] = 'food';
-            })
-            grid.mines.forEach(pos => {
-                newCells[pos[0]][pos[1]] = 'mine';
-            })
+            const newSnake = getUpdatedSnake(grid.cells, grid.snake, nextPosition);
+            const newFood = getUpdatedFood(grid.food, nextPosition);
+            const newCells = getUpdatedCells(grid.cells, newSnake, newFood, grid.mines);
 
             return {
                 ...grid,
@@ -122,7 +108,7 @@ export function gridReducer(grid, action) {
     }
 }
 
-function getNextPostion(i, j, rows, cols, direction) {
+function getNextPosition(i, j, rows, cols, direction) {
     if (direction === Directions.UP) {
         i = i - 1;
     } else if (direction === Directions.DOWN) {
@@ -140,6 +126,52 @@ function getNextPostion(i, j, rows, cols, direction) {
 
 function isSnakeTryingToReverse(snake, direction) {
     if (snake.length <= 1) return false;
-    const nextPostion = getNextPostion(...snake[snake.length - 1], gridRows, gridCols, direction);
-    return nextPostion[0] === snake[snake.length - 2][0] && nextPostion[1] === snake[snake.length - 2][1];
+    const nextPosition = getNextPosition(...snake[snake.length - 1], gridRows, gridCols, direction);
+    return nextPosition[0] === snake[snake.length - 2][0] && nextPosition[1] === snake[snake.length - 2][1];
+}
+
+function isSnakeGoingToDie(cells, nextPosition) {
+    if (cells[nextPosition[0]][nextPosition[1]] === 'snake' || cells[nextPosition[0]][nextPosition[1]] === 'mine')
+        return true;
+    return false;
+}
+
+function isNextFood(cells, nextPosition) {
+    if (cells[nextPosition[0]][nextPosition[1]] === 'food')
+        return true;
+    return false;
+}
+
+function getUpdatedCells(cells, snake, food, mines) {
+    const newCells = cells.map((cellRows) => cellRows.map(() => 'empty'));
+
+    // update cells
+    snake.forEach(pos => {
+        newCells[pos[0]][pos[1]] = 'snake';
+    });
+    food.forEach(pos => {
+        newCells[pos[0]][pos[1]] = 'food';
+    })
+    mines.forEach(pos => {
+        newCells[pos[0]][pos[1]] = 'mine';
+    })
+
+    return newCells;
+}
+
+function getUpdatedSnake(cells, snake, nextPosition) {
+    const nextIsFood = isNextFood(cells, nextPosition);
+
+    // remove the tail. but if snake ate food don't remove it
+    const newSnake = snake.filter((pos, i) => (i !== 0 || nextIsFood));
+
+    newSnake.push(nextPosition)
+
+    return newSnake;
+}
+
+function getUpdatedFood(food, nextPosition) {
+    // if snake head is on food then remove food from there
+    const newFood = food.filter(pos => (pos[0] !== nextPosition[0]) || (pos[1] !== nextPosition[1]));
+    return newFood;
 }
