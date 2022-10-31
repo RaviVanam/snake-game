c53c8fa38c402d46ac6ba95c4ba8f10d2b285888
Ravi Vanam <ravivanam9922@gmail.com>
2022-10-31 19:32:49 +0530

    remove snakeSet and randomly drop mines

--- src/initialValues.js
+++ src/initialValues.js
@@ -6,20 +6,19 @@ export const gridCols = 40;
 const snakePositions = [[10, 20], [11, 20], [12, 20], [13, 20], [14, 20], [15, 20], [16, 20], [17, 20], [18, 20]].reverse();
 
 const initialCells = new Array(gridRows).fill(null).map(() => new Array(gridCols).fill('empty'));
-const initialSnakeSet = new Array(gridRows).fill(null).map(() => new Array(gridCols).fill(false));
 snakePositions.forEach(([i, j]) => {
     initialCells[i][j] = 'snake';
-    initialSnakeSet[i][j] = true;
 });
 
 export const initialState = {
     cells: initialCells,
     direction: Directions.UP,
     snake: snakePositions,
-    snakeSet: initialSnakeSet,
     food: [],
+    mines: [],
     snakeSpeed: 10,
     foodSpeed: 10,
+    mineSpeed: 10,
     endGame: true,
     firstGame: true,
     gameOverScreen: false,
