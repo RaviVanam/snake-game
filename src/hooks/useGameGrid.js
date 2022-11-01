4a5bd59b5c08a27f8e7d1979b0868cb3a90c9e46
Ravi Vanam <ravivanam9922@gmail.com>
2022-11-01 17:17:32 +0530

    use custom hook useGameEffects and store handlers with useMemo

--- src/hooks/useGameGrid.js
+++ src/hooks/useGameGrid.js
@@ -1,167 +1,88 @@
-import { useReducer } from "react";
-import { useEffect } from "react";
+import { useReducer, useMemo } from "react";
+import useGameEffects from "./useGameEffects.js";
 import { gridReducer } from "../reducers.js";
 import { initialState, transitionDuration } from "../initialValues";
 
 export default function useGameGrid() {
     const [grid, dispatchGrid] = useReducer(gridReducer, initialState);
 
-    const handlers = {
-        handleMove() {
-            dispatchGrid({
-                type: 'moved',
-            })
-        },
+    const handlers = useMemo(() => {
+        return {
+            handleMove() {
+                dispatchGrid({
+                    type: 'moved',
+                })
+            },
 
-        handlePlay() {
-            dispatchGrid({
-                type: 'startGame',
-                snakeSpeed: grid.snakeSpeed,
-                foodSpeed: grid.foodSpeed,
-            })
-        },
+            handlePlay() {
+                dispatchGrid({
+                    type: 'startGame',
+                })
+            },
 
-        handleDirectionChanged(newDirection) {
-            dispatchGrid({
-                type: 'directionChanged',
-                newDirection: newDirection,
-            })
-        },
+            handleDirectionChanged(newDirection) {
+                dispatchGrid({
+                    type: 'directionChanged',
+                    newDirection: newDirection,
+                })
+            },
 
-        handlePlaceFood() {
-            dispatchGrid({
-                type: 'placeFood',
-            })
-        },
+            handlePlaceFood() {
+                dispatchGrid({
+                    type: 'placeFood',
+                })
+            },
 
-        handleRemoveFood() {
-            dispatchGrid({
-                type: 'removeFood',
-            })
-        },
+            handleRemoveFood() {
+                dispatchGrid({
+                    type: 'removeFood',
+                })
+            },
 
-        handlePlaceMine() {
-            dispatchGrid({
-                type: 'placeMine',
-            })
-        },
+            handlePlaceMine() {
+                dispatchGrid({
+                    type: 'placeMine',
+                })
+            },
 
-        handleRemoveMine() {
-            dispatchGrid({
-                type: 'removeMine',
-            })
-        },
+            handleRemoveMine() {
+                dispatchGrid({
+                    type: 'removeMine',
+                })
+            },
 
-        handleSnakeSpeedChange(e) {
-            dispatchGrid({
-                type: 'snakeSpeedChanged',
-                speed: e.target.value,
-            })
-        },
+            handleSnakeSpeedChange(e) {
+                dispatchGrid({
+                    type: 'snakeSpeedChanged',
+                    speed: e.target.value,
+                })
+            },
 
-        handleFoodSpeedChange(e) {
-            dispatchGrid({
-                type: 'foodSpeedChanged',
-                speed: e.target.value,
-            })
-        },
+            handleFoodSpeedChange(e) {
+                dispatchGrid({
+                    type: 'foodSpeedChanged',
+                    speed: e.target.value,
+                })
+            },
 
-        handleMineSpeedChange(e) {
-            dispatchGrid({
-                type: 'mineSpeedChanged',
-                speed: e.target.value,
-            })
-        },
+            handleMineSpeedChange(e) {
+                dispatchGrid({
+                    type: 'mineSpeedChanged',
+                    speed: e.target.value,
+                })
+            },
 
-        handlePlayAgain() {
-            dispatchGrid({
-                type: 'resetGame',
-            })
+            handlePlayAgain() {
+                dispatchGrid({
+                    type: 'resetGame',
+                })
 
-            setTimeout(handlers.handlePlay, transitionDuration);
-        },
-    }
+                setTimeout(handlers.handlePlay, transitionDuration);
+            },
+        }
+    }, [])
 
-    const effects = {
-        addKeydownListener() {
-            const handleKeyPressedEvent = (e) => {
-                e.preventDefault();
-                if (e.keyCode >= 37 && e.keyCode <= 40) // keycodes 37 to 40 match arrow keys
-                    handlers.handleDirectionChanged(e.key);
-            }
-            window.addEventListener('keydown', handleKeyPressedEvent);
-
-            return () => {
-                window.removeEventListener('keydown', handleKeyPressedEvent);
-            }
-        },
-
-        autoMoveSnake() {
-            if (grid.endGame) return () => { };
-            const id = setInterval(() => {
-                handlers.handleMove();
-            }, 500 / grid.snakeSpeed)
-
-            return () => {
-                clearInterval(id);
-            }
-        },
-
-        autoGenerateFood() {
-            if (grid.endGame) return () => { };
-
-            const id = setInterval(() => {
-                handlers.handlePlaceFood();
-            }, 5000 / grid.foodSpeed)
-
-            return () => {
-                clearInterval(id);
-            }
-        },
-
-        autoRemoveFood() {
-            if (grid.endGame) return () => { };
-
-            const id = setInterval(() => {
-                handlers.handleRemoveFood();
-            }, 30000 / grid.foodSpeed)
-
-            return () => {
-                clearInterval(id);
-            }
-        },
-
-        autoDropMines() {
-            if (grid.endGame) return () => { };
-
-            const id = setInterval(() => {
-                handlers.handlePlaceMine();
-            }, 7000 / grid.mineSpeed)
-
-            return () => {
-                clearInterval(id);
-            }
-        },
-
-        autoRemoveMines() {
-            if (grid.endGame) return () => { };
-
-            const id = setInterval(() => {
-                handlers.handleRemoveMine();
-            }, 100000 / grid.mineSpeed)
-
-            return () => {
-                clearInterval(id);
-            }
-        },
-    }
-
-    useEffect(effects.addKeydownListener, []);
-    useEffect(effects.autoMoveSnake, [grid.endGame, grid.snakeSpeed]);
-    useEffect(effects.autoGenerateFood, [grid.endGame, grid.foodSpeed]);
-    useEffect(effects.autoRemoveFood, [grid.endGame, grid.foodSpeed]);
-    useEffect(effects.autoDropMines, [grid.endGame, grid.mineSpeed]);
-    useEffect(effects.autoRemoveMines, [grid.endGame, grid.mineSpeed]);
+    useGameEffects(grid, handlers);
 
     return [grid, handlers];
 }
