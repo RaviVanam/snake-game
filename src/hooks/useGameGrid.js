f615fc4b6a56d4cdc372af9207325f92f05144b6
Ravi Vanam <ravivanam9922@gmail.com>
2022-11-01 11:29:14 +0530

    create useGameGrid hook

---
+++ src/hooks/useGameGrid.js
@@ -0,0 +1,87 @@
+import { useReducer } from "react";
+import { gridReducer } from "../reducers.js";
+import { initialState, transitionDuration } from "../initialValues";
+
+export default function useGameGrid() {
+    const [grid, dispatchGrid] = useReducer(gridReducer, initialState);
+
+    const handlers = {
+        handleMove() {
+            dispatchGrid({
+                type: 'moved',
+            })
+        },
+
+        handlePlay() {
+            dispatchGrid({
+                type: 'startGame',
+                snakeSpeed: grid.snakeSpeed,
+                foodSpeed: grid.foodSpeed,
+            })
+        },
+
+        handleDirectionChanged(newDirection) {
+            dispatchGrid({
+                type: 'directionChanged',
+                newDirection: newDirection,
+            })
+        },
+
+        handlePlaceFood() {
+            dispatchGrid({
+                type: 'placeFood',
+            })
+        },
+
+        handleRemoveFood() {
+            dispatchGrid({
+                type: 'removeFood',
+            })
+        },
+
+        handlePlaceMine() {
+            dispatchGrid({
+                type: 'placeMine',
+            })
+        },
+
+        handleRemoveMine() {
+            dispatchGrid({
+                type: 'removeMine',
+            })
+        },
+
+        handleSnakeSpeedChange(e) {
+            dispatchGrid({
+                type: 'snakeSpeedChanged',
+                speed: e.target.value,
+            })
+        },
+
+        handleFoodSpeedChange(e) {
+            dispatchGrid({
+                type: 'foodSpeedChanged',
+                speed: e.target.value,
+            })
+        },
+
+        handleMineSpeedChange(e) {
+            dispatchGrid({
+                type: 'mineSpeedChanged',
+                speed: e.target.value,
+            })
+        },
+
+        handlePlayAgain() {
+            dispatchGrid({
+                type: 'resetGame',
+            })
+
+            setTimeout(this.handlePlay, transitionDuration);
+        },
+    }
+
+    handlers.handlePlayAgain.bind(handlers);
+
+    return [grid, handlers];
+}
